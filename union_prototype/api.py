22127055da8256e5c2042f60124252a2118099fc
nofructose <39570394+paulbry@users.noreply.github.com>
2018-11-25 11:02:50 -0500

    Minor changes (moving to Linux dev)

--- union_prototype/api.py
+++ union_prototype/api.py
@@ -8,11 +8,17 @@ class Parallel(Resource):
     def __init__(self):
         pass
 
-    def get(self, obj_id=None, test=None):
-        if test is None:
+    def get(self, obj_id=None, system=None):
+        # GET /parallel
+        if obj_id is None:
+            return {'hello': 'world'}
+
+        # GET /parallel/obj_id/
+        if system is None:
             return {'hello': obj_id}
+        # GET /parallel/obj_id/system
         else:
-            return {'test': test}
+            return {'test': system}
 
     def put(self):
         pass
