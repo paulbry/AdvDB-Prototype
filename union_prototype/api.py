47a69b2038e38753736a4e6fdef3ef50ae7323fe
paulbry <39570394+paulbry@users.noreply.github.com>
2018-12-05 10:28:31 -0500

    Updated for Cloud interactions

--- union_prototype/api.py
+++ union_prototype/api.py
@@ -1,7 +1,10 @@
+# system
+import os
 # 3rd party
 from flask import request, Flask
 from flask_restful import Resource, Api, reqparse
 # project
+from union_prototype import cloud_interface as up_ci
 from union_prototype import db_interface
 
 ###########################################################################
@@ -12,8 +15,15 @@ app = Flask(__name__)
 # noinspection PyTypeChecker
 api = Api(app)
 
+# TODO: clearly document in a forward facing way
+# At this moment the newObjID is more or less required for a number of interactions
+# This can/should be corrected after we have finalized the hash & split functionality
+# as these directly play into the naming conventions
 parser = reqparse.RequestParser()
-parser.add_argument('task')
+parser.add_argument('task')  # Temporary, used in a few tests and should be removed
+parser.add_argument('removeAfter', type=bool, default=False)
+parser.add_argument('newObjID', default=None)
+parser.add_argument('download', type=bool, default=False)
 
 
 class MetaData(Resource):
@@ -33,6 +43,7 @@ class MetaData(Resource):
     def get(self, obj_id=None):
         # GET /parallel
         if obj_id is None:
+            # TODO: general TODO across api.py is add error code to return tuple
             return {'valid_parent_ids': self.par_db.api_get_all_id()}
         # GET /parallel/obj_id/
         else:
@@ -94,8 +105,9 @@ class Parallel(Resource):
     ###########################################################################
     # REST API
     #
-    # GET -
-    # PUT -
+    # GET - Copy file from mounted files system to mounted system and
+    #       query the DB for information related to objects
+    # PUT - Upload file from cloud to file system
     ###########################################################################
     def get(self, obj_id=None):
         # GET /parallel
@@ -120,18 +132,29 @@ class Cloud(Resource):
     ###########################################################################
     # REST API
     #
-    # GET -
-    # PUT -
+    # GET - Download file from cloud system to local file system as well as
+    #       query the DB for information related to objects
+    # PUT - Upload file from mounted files system to cloud
     ###########################################################################
     def get(self, obj_id=None):
+        args = parser.parse_args()
+
         # GET /cloud
         if obj_id is None:
             return {'valid_parent_ids': self.cld_db.api_get_all_id(True)}
         # GET /cloud/obj_id/
         else:
+            if args.Download:
+                tar_par_loc = (self.cld_db.safe_query_value('objectID', obj_id, 'parallelLoc'))[0]
+                og_cloud_vendor = (self.cld_db.safe_query_value('objectID', obj_id, 'cloudVendor'))[0]
+                og_cloud_loc = (self.cld_db.safe_query_value('objectID', obj_id, 'cloudLoc'))[0]
+                execute_cloud_get(obj_id, og_cloud_vendor, og_cloud_loc, tar_par_loc, obj_id,
+                                  args.removeAfter)
             return self.par_db.api_get_object(str(obj_id))
 
     def put(self, obj_id=None, cloud_vendor=None, cloud_loc=None):
+        args = parser.parse_args()
+
         # PUT /cloud
         if obj_id is None:
             return {'unsupported': 'A valid objectID must be provided'}
@@ -146,39 +169,77 @@ class Cloud(Resource):
                 # PUT /cloud/obj_id/cloud_vendor
                 return {'error': 'no cloud location can be established'}
 
-            if execute_cloud_put(obj_id, vend, cloc):
-                pass  # Success
-            else:
-                pass  # Failure
+            new_obj_id = args.newObjID
+            if args.newObjID is None:
+                # TODO: determine plan for creating obj_id
+                pass
 
-        # TODO: re-implement method for writing new object to DB
-        #       remember that object may be created based upon
-        #       decided PUT/GET context
-        # TODO: clean up and supply useful information
-        return {'PUT details': {'cloudVendor': vend,
-                                'cloudLoc': cloc}}
+            og_par_loc = (self.cld_db.safe_query_value('objectID', obj_id, 'parallelLoc'))[0]
+            file_hash = "TEST"   # TODO: implement has support
+            parent = None  # TODO: implement concept of parent? (maybe if split)
+
+            b, id = execute_cloud_put(og_obj_id=obj_id, og_par_loc=og_par_loc,
+                                      tar_obj_id=new_obj_id, tar_cloud_vendor=vend,
+                                      tar_cloud_loc=cloc, remove_after=args.removeAfter)
+            if b:
+                if args.removeAfter:
+                    self.cld_db.safe_delete_entry('objectID', obj_id)
+                self.cld_db.api_insert_event(new_obj_id, og_par_loc, cloc, file_hash,
+                                             parent, vend)
+                return id  # Success
+            else:
+                return id  # Failure
 
 
 def execute_cloud_get(og_obj_id, og_cloud_vendor, og_cloud_loc,
-                      tar_par_loc, remove_after):
-    # TODO: download file from CLOUD
-    pass
+                      tar_par_loc, tar_obj_id, remove_after):
+    """ Download file from cloud system to local file system """
+    if og_cloud_vendor == 'gcloud':
+        up_ci.gcloud_download_blob(og_cloud_loc, og_obj_id, tar_par_loc + "/" + tar_obj_id)
+        if remove_after:
+            up_ci.gcloud_delete_blob(og_cloud_loc, og_obj_id)
+    elif og_cloud_vendor == 'aws':
+        # TODO: add aws support and potentially reformat via adapter pattern?
+        return False, {'error': 'AWS is currently unsupported'}
+    else:
+        return False, {'error': 'Unsupported cloud vendor {0} provided'.format(
+            og_cloud_vendor
+        )}
+
+    return False, {'error': 'Unable to run execute_cloud_get'}
 
 
 def execute_cloud_put(og_obj_id, og_par_loc,
                       tar_obj_id, tar_cloud_vendor, tar_cloud_loc,
                       remove_after):
-    # TODO: upload file to CLOUD
-    pass
+    """ Upload file from mounted files system to cloud """
+
+    if tar_cloud_vendor == 'gcloud':
+        up_ci.gcloud_create_bucket(tar_cloud_loc)
+        up_ci.gcloud_upload_blob(tar_cloud_loc, og_par_loc + "/" + og_obj_id,
+                                 tar_obj_id)
+        if remove_after:
+            os.remove(og_par_loc + "/" + og_obj_id)
+    elif tar_cloud_vendor == 'aws':
+        # TODO: add aws support and potentially reformat via adapter pattern?
+        return False, {'error': 'AWS is currently unsupported'}
+    else:
+        return False, {'error': 'Unsupported cloud vendor {0} provided'.format(
+            tar_cloud_vendor
+        )}
+
+    return False, {'error': 'Unable to run execute_cloud_put'}
 
 
 def execute_parallel_get(og_obj_id, tar_par_loc, remove_after):
+    """ Copy file from mounted files system to mounted system """
     # TODO: download file from PARALLEL
     pass
 
 
 def execute_parallel_put(og_obj_id, og_cloud_vendor, og_cloud_loc,
                          tar_obj_id, tar_par_loc, remove_after):
+    """ Upload file from cloud to file system """
     # TODO: upload file to PARALLEL
     pass
 
@@ -202,8 +263,7 @@ def main(debug, db_url, db_port):
                      resource_class_kwargs={
                          'db_url': db_url,
                          'db_port': db_port
-                     }
-                     )
+                     })
 
     # MetaData (PUT/DELETE entries in DB only!) resource
     api.add_resource(MetaData, '/meta',
@@ -211,8 +271,7 @@ def main(debug, db_url, db_port):
                      resource_class_kwargs={
                          'db_url': db_url,
                          'db_port': db_port
-                     }
-                     )
+                     })
 
     app.run(debug=debug)
 
